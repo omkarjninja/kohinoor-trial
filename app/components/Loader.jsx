05a1e15cb2503043dd2ee69ffd0bc3858c755b0d
Omkar Jadhav <omkarjadhav@Omkars-MacBook-Air.local>
2025-02-23 01:57:32 +0530

    first commit

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
-import  "./loader.css";
+import  "./Loader.css";
 const Container = styled(motion.div)`
   position: absolute;
 
